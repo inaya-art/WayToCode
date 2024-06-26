159691cf4165b20a83c9b4b533bf74102236b145
Inaya Dar <inayadar@gmail.com>
2024-06-26 09:10:43 -0500

    Initial Commit

---
+++ WayToCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WayToCode
+//
+//  Created by Inaya Dar on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WayToCode/WayToCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WayToCodeApp.swift
+//  WayToCode
+//
+//  Created by Inaya Dar on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct WayToCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
